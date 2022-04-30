97cdb1018ace137a4132638b43f2709b71939e56
Danial <imdaniald@gmail.com>
2022-04-30 22:01:22 +0430

    Fix profile repository json bug

--- server/app/feeds/repository/profile_repository.go
+++ server/app/feeds/repository/profile_repository.go
@@ -5,16 +5,16 @@ import (
 )
 
 type Profile struct {
-	ID             uint      `json:"id,omitempty"`
-	Name           string    `json:"name,omitempty"`
-	Username       string    `json:"username,omitempty"`
-	Bio            string    `json:"bio,omitempty"`
+	ID             uint      `json:"id"`
+	Name           string    `json:"name"`
+	Username       string    `json:"username"`
+	Bio            string    `json:"bio"`
 	Image          string    `json:"image,omitempty"`
 	Birthday       string    `json:"birthday,omitempty"`
 	Suspended      bool      `json:"suspended,omitempty"`
 	Official       bool      `json:"official,omitempty"`
 	Followed       bool      `json:"followed" gorm:"-"`
-	FollowersCount int       `json:"followers_count,omitempty"`
-	FollowingCount int       `json:"following_count,omitempty"`
+	FollowersCount int       `json:"followers_count"`
+	FollowingCount int       `json:"following_count"`
 	CreatedAt      time.Time `json:"created_at"`
 }
