c3711d832288b2c3b14ad3e066efd8be2581de9f
Danial <imdaniald@gmail.com>
2022-05-02 17:52:05 +0430

    Fix search followersCount json key

--- server/app/feeds/repository/search_profile_repository.go
+++ server/app/feeds/repository/search_profile_repository.go
@@ -6,5 +6,5 @@ type SearchProfile struct {
 	Username       string `json:"username"`
 	Image          string `json:"image"`
 	Official       bool   `json:"official,omitempty"`
-	FollowersCount int    `json:"followersCount"`
+	FollowersCount int    `json:"followers_count"`
 }
